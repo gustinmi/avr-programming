c3dd49ca37d7afc58e15025e0e2c3289025a28c3
U-GUSTIN-PC\gustin <gustin@gustin-pc.svz.sigov.si>
2018-09-27 09:51:38 +0200

    eeprom and bootloader

---
+++ eeprom/eeprom.ino
@@ -0,0 +1,26 @@
+#include <EEPROM.h>
+
+void setup() {
+
+    Serial.begin(9600); 
+
+  //if (EEPROM.read(0) != 0xAA) {
+    EEPROM.write(1, 12);
+    EEPROM.write(2, 34);
+    EEPROM.write(0, 0xAA);
+
+    Serial.print("Done setup");
+  //}else{
+    Serial.println("Value is");
+
+    Serial.print(EEPROM.read(0));
+  //}
+
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
